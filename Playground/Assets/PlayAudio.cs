6b55ee38180f6205e8247104b9b2a93810df10d5
Alexanity <80948589+Alexanity@users.noreply.github.com>
2023-05-06 23:18:25 +0300

    Fixing the audio?

--- Playground/Assets/PlayAudio.cs
+++ Playground/Assets/PlayAudio.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class PlayAudio : MonoBehaviour
 {
+    public float cooldownTime = 5f;
     private AudioSource audioSource;
     private bool hasPlayed = false;
+    private float cooldownTimer;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        audioSource.Stop();
+    }
+
+    private void Update()
+    {
+        if (hasPlayed)
+        {
+            cooldownTimer += Time.deltaTime;
+            if (cooldownTimer >= cooldownTime)
+            {
+                hasPlayed = false;
+                cooldownTimer = 0f;
+            }
+        }
     }
 
     private void OnParticleCollision(GameObject other)
@@ -19,10 +33,6 @@ public class PlayAudio : MonoBehaviour
         {
             audioSource.Play();
             hasPlayed = true;
-        }
-        else
-        {
-            hasPlayed = false;
         }
     }
 }
